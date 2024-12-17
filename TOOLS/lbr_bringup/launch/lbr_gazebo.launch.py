c1a0042ad5b051163cd93aef73219c658211b980
Alessandro Puglisi <puglisialessandro27@gmail.com>
2024-12-17 11:10:11 +0100

    lbr_bringup to install launch

--- TOOLS/lbr_bringup/launch/lbr_gazebo.launch.py
+++ TOOLS/lbr_bringup/launch/lbr_gazebo.launch.py
@@ -7,14 +7,31 @@ from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+from launch_ros.actions import Node
+
 launch_dir_path = os.path.dirname(os.path.realpath(__file__))
 default_wheel_mode = 'true'
+
+#default_data_analysis = 'false'
+#data_analysis = False
 
 def generate_launch_description():
 
     wheel_mode_arg = DeclareLaunchArgument('wheel_mode',
                                       default_value = default_wheel_mode)
     
+    #data_analysis_arg = DeclareLaunchArgument('data_analysis',
+    #                                  default_value = default_data_analysis)
+    
+    #data_analysis = LaunchConfiguration('data_analysis')
+
+    analysis_node = Node(
+        package='lbr_data_analysis',
+        executable='lbr_data_analysis',
+        name='lbr_data_analysis',
+        output='screen'
+    )
+
     lbr_controller = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             launch_dir_path + '/lbr_controller.launch.py']
@@ -30,8 +47,11 @@ def generate_launch_description():
         launch_arguments = {'wheel_mode': LaunchConfiguration('wheel_mode')}.items(),
     )
 
+
     return LaunchDescription([
+        analysis_node,
         wheel_mode_arg,
+        #data_analysis_arg,
         lbr_controller,
-        simulation_nodes
+        simulation_nodes,
     ])
