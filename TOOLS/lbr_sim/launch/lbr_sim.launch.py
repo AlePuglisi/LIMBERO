1b0d5d60bfd0936269656ffb1395108b9cbfa39d
Alessandro Puglisi <puglisialessandro27@gmail.com>
2024-12-19 12:52:16 +0100

    tentative custom joint controller, BUILD FAIL

--- TOOLS/lbr_sim/launch/lbr_sim.launch.py
+++ TOOLS/lbr_sim/launch/lbr_sim.launch.py
@@ -102,7 +102,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        arguments=['joint_trajectory_controller', '--controller-manager',
+        arguments=['custom_joint_trajectory_controller', '--controller-manager',
                    '/controller_manager'],
     )
 
